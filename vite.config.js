eb0f2c4b2b2ef948c1f7e4cb2cd6631e885be3bd
Darkhunter3210 <matthewcheung3003@gmail.com>
2026-08-02 23:07:13 -0400

    changed vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/nail-salon/' : '/',
+  base: '/',
   server: {port: 5173, strictPort: true}
 }))
